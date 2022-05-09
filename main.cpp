875845156cd9c9db70c3ea01708912e44a96064d
kpalaciosm02 <kpalaciosm02@gmail.com>
2022-05-09 15:49:49 -0600

    Changes to tesla.svg

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int main(){
     //Leer XML
+
     file<> file("test2.svg"); // Lee y carga el archivo en memoria
     xml_document<> myDoc; //Raíz del árbol DOM
     myDoc.parse<0>(file.data()); //Parsea el XML en un DOM
